96500acb3f94e1ddff58041a62b9e18fc222bb15
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2024-12-01 21:15:31 -0800

    models creation

--- src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
+++ src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
@@ -1,8 +1,70 @@
 package com.teamtreehouse.blog.model;
 
+import com.github.slugify.Slugify;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlogEntry {
+    // Adding necessary fields
+    private String slug;
+    private String title;
+    private String content;
+    private LocalDateTime creationDate;
+    private List<Comment> comments;
+
+    public BlogEntry(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.creationDate = LocalDateTime.now();
+        this.comments = new ArrayList<>();
+        try {
+            Slugify slugify = new Slugify();
+            slug = slugify.slugify(title);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    // Implementation of a method that manages comments
     public boolean addComment(Comment comment) {
         // Store these comments!
-        return false;
+        return comments.add(comment);
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
     }
 }
--- src/main/java/com/teamtreehouse/blog/model/Comment.java
+++ src/main/java/com/teamtreehouse/blog/model/Comment.java
@@ -1,4 +1,39 @@
 package com.teamtreehouse.blog.model;
 
+
+import java.time.LocalDateTime;
+
 public class Comment {
+    // Adding fields
+    private String author;
+    private String content;
+    private LocalDateTime creationDate;
+
+    public Comment(String author, String content) {
+        this.author = author;
+        this.content = content;
+        this.creationDate = LocalDateTime.now();
+    }
+
+    // Getters
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    // Setters
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
