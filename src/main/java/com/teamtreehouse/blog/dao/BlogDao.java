815994fef5135f34e564c1346513ab230c6d6725
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2024-12-01 22:26:34 -0800

    dao implementation

--- src/main/java/com/teamtreehouse/blog/dao/BlogDao.java
+++ src/main/java/com/teamtreehouse/blog/dao/BlogDao.java
@@ -5,7 +5,12 @@ import com.teamtreehouse.blog.model.BlogEntry;
 import java.util.List;
 
 public interface BlogDao {
+    // Add entry
     boolean addEntry(BlogEntry blogEntry);
+
+    // Implementation of a way to find all entries
     List<BlogEntry> findAllEntries();
+
+    // Adds the ability to find one by slug
     BlogEntry findEntryBySlug(String slug);
 }
---
+++ src/main/java/com/teamtreehouse/blog/dao/SimpleBlogDao.java
@@ -0,0 +1,37 @@
+package com.teamtreehouse.blog.dao;
+
+import com.teamtreehouse.blog.model.BlogEntry;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SimpleBlogDao implements BlogDao{
+    private List<BlogEntry> entries;
+
+    public SimpleBlogDao() {
+        this.entries = new ArrayList<>();
+    }
+
+    // Implement addEntry method to store new blog entries
+    @Override
+    public boolean addEntry(BlogEntry blogEntry) {
+        return entries.add(blogEntry);
+    }
+
+    // Develop findAllEntries method to retrieve all blog posts
+    @Override
+    public List<BlogEntry> findAllEntries() {
+        return new ArrayList<>(entries);
+    }
+
+    // Implement findEntryBySlug to fetch a specific blog entry
+    @Override
+    public BlogEntry findEntryBySlug(String slug) {
+        return entries.stream()
+                .filter(entry -> entry.getSlug().equals(slug))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+}
